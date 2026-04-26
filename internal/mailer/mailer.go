3e25aee98375e7158087f5d1457726eeb6a95f88
Vidwa De Seram <vidwanirmaldeseram@gmail.com>
2026-04-26 09:19:29 +0530

    feat: add auth transport contracts
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ internal/mailer/mailer.go
@@ -0,0 +1,27 @@
+package mailer
+
+import (
+	"fmt"
+	"log/slog"
+	"net/smtp"
+
+	"github.com/vidwadeseram/go-multi-tenant-auth-template/internal/config"
+)
+
+type Mailer struct {
+	settings *config.Settings
+}
+
+func New(settings *config.Settings) *Mailer {
+	return &Mailer{settings: settings}
+}
+
+func (m *Mailer) Send(recipient, subject, body string) error {
+	addr := fmt.Sprintf("%s:%d", m.settings.SMTPHost, m.settings.SMTPPort)
+	message := []byte(fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s", recipient, subject, body))
+	if err := smtp.SendMail(addr, nil, m.settings.SMTPSender, []string{recipient}, message); err != nil {
+		slog.Warn("failed to send email", "recipient", recipient, "error", err)
+		return err
+	}
+	return nil
+}
